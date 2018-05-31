f2efa99e5804ad7d5047528985bded6c88f81966
Yuusuke Nagata <yuusukenagata@YuusukesMacBook-Pro.local>
2018-05-31 18:13:24 +0900

    update_incremental_search

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,6 @@
 $(function () {
 // インクリメンタルサーチで対象ユーザがいる場合の機能
-var user_list = $("#js-user-search-result");
+var user_list = $("#user-search-result");
 function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
@@ -17,9 +17,9 @@ function appendNoUser(user) {
   user_list.append(html);
 }
 // 検索したユーザをチャットメンバー欄に追加する機能
-var group_list = $(".chat-group-users.js-add-user")
+var group_list = $("#chat-group-users")
 function appendGroup(userId, userName) {
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-'${userId}'>
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${userId}'>
                 <input name='group[user_ids][]' type='hidden' value='${userId}'>
                 <p class='chat-group-user__name'>${userName}</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -27,8 +27,8 @@ function appendGroup(userId, userName) {
   group_list.append(html)
 }
 // インクリメンタルサーチ機能
-  $('.js-user-search-field').on('keyup', function() {
-    var input = $('.js-user-search-field').val();
+  $('#user-search-field').on('keyup', function() {
+    var input = $('#user-search-field').val();
     $.ajax({
       type: 'GET',
       url: '/users/',
@@ -36,7 +36,7 @@ function appendGroup(userId, userName) {
       dataType: 'json'
     })
     .done(function(users) {
-      $('#js-user-search-result').empty();
+      $('#user-search-result').empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
           appendUser(user);
@@ -51,14 +51,14 @@ function appendGroup(userId, userName) {
     })
   });
 // 検索ユーザの追加ボタンを押してチャットメンバー欄に追加する機能
-  $("#js-user-search-result").on('click','.chat-group-user__btn--add', function() {
+  $("#user-search-result").on('click','.chat-group-user__btn--add', function() {
     var userId = $(this).data("userId");
     var userName = $(this).data("userName");
     appendGroup(userId, userName);
     $(this).parent().remove();
   });
 // チャットメンバーの削除ボタンを押してユーザーを削除する機能
-  $(".chat-group-users.js-add-user").on('click','.chat-group-user__btn--remove', function() {
-    $(this).parent().remove();
+  $("#chat-group-users").on('click','.chat-group-user__btn--remove', function() {
+    $(this).parent().empty();
   });
 });
