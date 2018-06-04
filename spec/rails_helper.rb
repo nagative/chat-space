c5009c4050ee94e93ba1d66638424ddd30c28bb3
Yuusuke Nagata <yuusukenagata@YuusukesMacBook-Pro.local>
2018-06-04 13:37:45 +0900

    update_gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
