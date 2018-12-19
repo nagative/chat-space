d17ed33484266edeb677b12885db4c0d8f95fc5b
Yuusuke Nagata <1y5u1s7u5k7e@gmail.com>
2018-12-19 13:39:08 +0900

    change_deploy_setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,13 @@ lock "~> 3.10.2"
 set :application, "chat-space"
 set :repo_url, "git@github.com:nagative/chat-space.git"
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads")
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, "2.3.1"
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/yuusukenagata/.ssh/TECH_EXPERT_AWS.pem']
+set :ssh_options, auth_methods: ["publickey"],
+                  keys: ["/Users/yuusukenagata/.ssh/TECH_EXPERT_AWS.pem"]
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
@@ -18,30 +18,30 @@ set :keep_releases, 5
 
 set :linked_files, %w{ config/secrets.yml }
 
-after 'deploy:publishing', 'deploy:restart'
+after "deploy:publishing", "deploy:restart"
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke "unicorn:restart"
   end
 
-  desc 'upload secrets.yml'
+  desc "upload secrets.yml"
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!("config/secrets.yml", "#{shared_path}/config/secrets.yml")
     end
   end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
+  before :starting, "deploy:upload"
+  after :finishing, "deploy:cleanup"
 end
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
 }
 
 # Default branch is :master
